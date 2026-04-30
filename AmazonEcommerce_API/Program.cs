bf874edd3ba9aeb5c67342c3685bd874f5ae8a3f
venkat <dell@DESKTOP-S8CP3CH>
2026-04-30 11:28:40 +0530

    userstory-123478-CreateCrudOperationsForEmployee-

--- AmazonEcommerce_API/Program.cs
+++ AmazonEcommerce_API/Program.cs
@@ -1,3 +1,9 @@
+using AmazonEcommerce_BusinessEntities.Interfaces;
+using AmazonEcommerce_DbConnectivity;
+using AmazonEcommerce_RepositoryLayer;
+using AmazonEcommerce_ServiceLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//We must register your context classes and pointing to your connectionstring
+//you should tell to ef core this context class is pointing to this database.
+builder.Services.AddDbContext<EmployeeContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeCodeFirstApproachDatabase")));
+builder.Services.AddDbContext<OrderContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("OrderCodeFirstApproachDatabase")));
+builder.Services.AddDbContext<DepartmentContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DepartmentCodeFirstApproachDatabase")));
+builder.Services.AddDbContext<RestaurantContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("RestaurantCodeFirstApproachDatabase")));
+
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+
 
 var app = builder.Build();
 
---
+++ AmazonEcommerce_BusinessEntities/Dtos/OrderDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmazonEcommerce_BusinessEntities.Dtos
+{
+    public class OrderDto
+    {
+        public int orderid { get; set; }
+        public string ordername { get; set; }
+        public string orderlocation { get; set; }
+
+    }
+}
